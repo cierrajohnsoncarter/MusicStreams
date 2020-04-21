0940b24b60999b6bf48f4b853c668505bd3b363c
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-04-20 20:40:43 -0400

    Ability to find top 5 streamed songs from library

--- script.py
+++ script.py
@@ -1,17 +1,26 @@
-songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
-playcounts = [78, 29, 44, 21, 89, 5]
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine",
+         "What's Going On", "Respect", "Good Vibrations",
+         "Blinding Lights", "Fast", "Everybody Dies in Their Nightmares",
+         "Snowchild"]
+playcounts = [78, 29, 44, 21, 89, 5, 100, 63, 105, 95]
 
-pairs = zip(songs, playcounts)
+pairs = list(zip(songs, playcounts))
 
-plays = {key:value for key, value in pairs}
+plays = {key: value for key, value in pairs}
 
-print(plays)
+# print(plays)
 
+# Songs with how many times they are streamed
 plays["Purple Haze"] = 1
-
 plays["Respect"] = 94
-
 
 library = {"The Best Songs": plays, "Sunday Feelings": {}}
 
-print(library)
+# print(library)
+
+
+def top_5_songs():
+    print(pairs.sort())
+
+
+#print(f"Your Top 5 Most Played Songs Are:{top_5}")
